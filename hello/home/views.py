eb6313ffa4b202400fe8e542d5f339e94a8746b9
Kasturi <kasturipatil2804@gmail.com>
2025-04-12 17:42:20 +0530

    Updated Files

--- hello/home/views.py
+++ hello/home/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render,HttpResponse
-
+from home.models import Task
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    context = {'success' : False , 'name': 'Kasturi'}
+    if request.method =="POST":
+        #handle the form
+        title = request.POST['title']
+        desc = request.POST['desc']
+        print(title,desc)
+        ins = Task(taskTitle=title, taskDesc=desc)
+        ins.save()
+        sucess=True
+        context = {'success' : True}
+
+    return render(request,'index.html',context)
 
 def tasks(request):
-    return render(request,'tasks.html')
+    allTasks=Task.objects.all()
+    context = {'tasks': allTasks}
+    return render(request,'tasks.html',context)
 
